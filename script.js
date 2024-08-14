6d956ef76acc9c27967904239cde0d315414006f
Zainab_Naz <arizakhanarizakhan869@gmail.com>
2024-08-14 13:22:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//chapter #03
+
+// var age = 18;
+
+// alert("I am 18 years old.")
+
+
+
+// var visitTime = 8;
+
+// alert("you have visited this site"+" "+visitTime+" "+"times.");
+
+
+// chapter #0
+
+
+// var birthYear 2001;
+
+
+// document.write("My birth year is"+" "+birthYear+"<br>");
+
+
+// document.write("Data type of my declared variable is number").
+
+
+ 
